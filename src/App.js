b93f4e73ccb71b8dfc237d940f30c4a88442ccc1
Gobinath <princegopinath42@gmail.com>
2023-08-13 19:29:45 +0530

    feat: search option

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
 function App() {
   const [news, setNews] = useState([]);
-
+  const [search, setSearch] = useState('react');
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
   const fetchNews = () => {
-    fetch("http://hn.algolia.com/api/v1/search?query=react")
+    fetch(url)
       .then(result => result.json())
       .then(data => setNews(data.hits))
       .catch(error => console.log(error));
@@ -13,10 +14,24 @@ function App() {
 
   useEffect(() => {
     fetchNews();
-  }, []); 
+  }, [url]); 
+ 
+  const handleClick = (e)=>{
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${search}`)
+  }
+  
   return (
     <div className="App">
       <h3>React News Feed</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={search} onChange={handleClick} />
+        <button >search</button>
+      </form>
       {news.map((n,i)=> (<p key={i}>{n.title}</p>))}
     </div>
   );
